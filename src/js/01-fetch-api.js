10a9eb32883ed773609c0162057a1de12b847495
MarynaFundament <marina.fundament@ukr.net>
2023-08-29 21:58:29 +0300

    mistake with markup pokemon - how to solve it??

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -13,45 +13,36 @@
  * - https://pokeapi.co/
  */
 
+
+
 import '../css/common.css';
-import pokemonCardTpl from '../templates/pokemon-card.hbs';
-import API from './api-service';
-import getRefs from './get-refs';
+import pokemonCardTpl from '../templates/pokemon-card.html';
 
-const refs = getRefs();
-
-refs.searchForm.addEventListener('submit', onSearch);
-
-function onSearch(e) {
-  e.preventDefault();
-
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
-
-  API.fetchPokemon(searchQuery)
-    .then(renderPokemonCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-}
-
-function renderPokemonCard(pokemon) {
+const r  = fetch ('https://pokeapi.co/api/v2/pokemon/2').then(response => {
+ return response.json();
+})
+.then (pokemon => {
+  console.log(pokemon);
   const markup = pokemonCardTpl(pokemon);
-  refs.cardContainer.innerHTML = markup;
-}
 
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
-}
+  
+  console.log (markup)
+})
+.catch(error => {
+  console.log(error)
+})
 
-// =========================================
 
-const url = 'https://newsapi.org/v2/everything?q=cars';
-const options = {
-  headers: {
-    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-  },
-};
+// const url = 'https://newsapi.org/v2/everything?q=cars'
+// const options = {
+//   headers: {
+//     Authorization: 'fe04efdf84594838934256a6f8030665'
+//   }
+// }
 
-fetch(url, options)
-  .then(r => r.json())
-  .then(console.log);
+
+// fetch (url, options)
+// .then(r  => r.json())
+// .then(console.log)
+
+
